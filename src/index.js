518687cbb40c5b71f06a7f9281295201f01a6cd2
kika-cavalher <ericavalher@gmail.com>
2022-04-02 10:39:58 -0300

    configure cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes/index')
 
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 routes(app)
 
